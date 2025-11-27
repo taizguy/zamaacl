3f01916a3d76257dd84daa995e7e3ebf59bf87b0
umer ijaz <uijaz389@gmail.com>
2025-11-27 23:02:38 +0500

    Fix PostCSS HTML parsing issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
-// vite.config.ts
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: null
+  },
   build: {
-    polyfillModulePreload: false
+    modulePreload: {
+      polyfill: false
+    }
   }
 })
