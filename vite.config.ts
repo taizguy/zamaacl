00892b15e87ddc7804ef8f08506619e528503506
umer ijaz <uijaz389@gmail.com>
2025-12-01 13:57:33 +0500

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: './postcss.config.js'
-  },
-  build: {
-    modulePreload: {
-      polyfill: false
-    }
-  }
+  plugins: [react()]
 })
